2d99508c2aa5b719dfbc3875c001f84b37f96172
choijungwoo493 <jwoo1016@gmail.com>
2023-05-19 09:01:16 +0900

    add files(content)

--- src/main/java/com/example/springedu2/Springedu2Application.java
+++ src/main/java/com/example/springedu2/Springedu2Application.java
@@ -2,8 +2,14 @@ package com.example.springedu2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.springedu2","springjpa","springrest"})
+@EnableJpaRepositories(basePackages = {"springjpa.exam.repository"})
+@EntityScan(basePackages = {"springjpa.exam.entity","springrest.exam.domain"})
 public class Springedu2Application {
 
     public static void main(String[] args) {
